fececee78af58b116ae14a453d46f5385117b892
Piero Ulloa <ulloapiero@gmail.com>
2018-08-16 13:10:36 -0500

    Añadir funcionestest

---
+++ test/tallerjunit/FuncionesTest.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tallerjunit;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author piero512
+ */
+public class FuncionesTest {
+    
+    public FuncionesTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of factorial method, of class Funciones.
+     */
+    @Test
+    public void testFactorial() {
+        System.out.println("factorial");
+        int number = 5;
+        int expResult = 120;
+        int result = Funciones.factorial(number);
+        assertTrue(expResult==result);
+        int number2 = -1;
+        try{
+            Funciones.factorial(number2);
+            fail("No se disparó la excepción de número negativo.");
+        } catch(UnsupportedOperationException e){
+            
+        }
+        // TODO review the generated test code and remove the default call to fail.
+    }
+    
+}
